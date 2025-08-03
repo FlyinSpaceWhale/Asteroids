50d0f94094434c44d9c023664fa0f0e51a6aca58
FlyinSpaceWhale <millerzajdel31@gmail.com>
2025-08-03 15:40:57 -0400

    added collision with asteroids

--- asteroids_uv/main.py
+++ asteroids_uv/main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -36,6 +37,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                sys.exit("Game over!")
         
         screen.fill("black")
         
