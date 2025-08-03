eb37655744e24fd41aece04fd67b1b6de4c4ed69
FlyinSpaceWhale <millerzajdel31@gmail.com>
2025-08-03 14:45:27 -0400

    drawing player on screen

--- asteroids_uv/main.py
+++ asteroids_uv/main.py
@@ -2,10 +2,33 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    
+    # infinite loop to redraw the game
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
